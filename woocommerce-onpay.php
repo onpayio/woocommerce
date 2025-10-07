1f18d241f16877f754d1b6bf540f0373d376f26f
ebbesmoeller <ebbe.moeller@team.blue>
2025-10-07 11:25:26 +0200

    Tagging new version 1.0.45

--- woocommerce-onpay.php
+++ woocommerce-onpay.php
@@ -30,7 +30,7 @@
 * Author URI: https://onpay.io/
 * Text Domain: wc-onpay
 * Domain Path: /languages
-* Version: 1.0.44
+* Version: 1.0.45
 **/
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -66,7 +66,7 @@ function init_onpay() {
     include_once __DIR__ . '/classes/gateway-klarna.php';
 
     class WC_OnPay extends WC_Payment_Gateway {
-        const PLUGIN_VERSION = '1.0.44';
+        const PLUGIN_VERSION = '1.0.45';
 
         const SETTING_ONPAY_GATEWAY_ID = 'gateway_id';
         const SETTING_ONPAY_SECRET = 'secret';
